1bf5c235e6c48c358c34d1da83faf2d218a8176b
Adam DeHaven <adehaven@priceweber.com>
2017-09-22 11:15:33 -0400

    Restructured files for easy installation

--- custom-gulp-commands/custom_gulp_commands.py
+++
@@ -1,7 +0,0 @@
-import sublime
-import sublime_plugin
-
-class GulpDeleteCacheOnKill(sublime_plugin.WindowCommand):
-    def run(self):
-        self.window.run_command("gulp_kill")
-        self.window.run_command("gulp_delete_cache")
